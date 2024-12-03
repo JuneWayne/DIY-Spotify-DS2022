0114f222780d97c50e134bee5362b914c54ed76f
Ethan Cao <wkt7ne@virginia.edu>
2024-12-03 14:57:37 -0500

    lambda update

--- fastapi/ingestor/app.py
+++ fastapi/ingestor/app.py
@@ -7,7 +7,7 @@ app.debug = True
 
 # Set the value of APP_BUCKET_NAME in the .chalice/config.json file.
 #S3_BUCKET = os.environ.get('APP_BUCKET_NAME', '')
-S3_BUCKET = 'esd4uq-dp1-spotify'
+S3_BUCKET = 'wkt7ne-dp1-spotify'
 
 @app.on_s3_event(bucket=S3_BUCKET, events=['s3:ObjectCreated:*'])
 def s3_handler(event):
